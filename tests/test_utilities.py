b22b109bf727fe3a070d17743dc148ffda22f1c4
Navin Karkera <navin@disroot.org>
2022-11-03 16:35:29 +0530

    feat: task to refresh xblock skills

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -16,7 +16,7 @@ from taxonomy.constants import ENGLISH
 from taxonomy.exceptions import TaxonomyAPIError
 from taxonomy.models import CourseSkills, JobSkills, Skill, Translation
 from test_utils import factories
-from test_utils.constants import COURSE_KEY, PROGRAM_UUID
+from test_utils.constants import COURSE_KEY, PROGRAM_UUID, USAGE_KEY
 from test_utils.mocks import MockCourse, MockProgram
 from test_utils.sample_responses.skills import SKILLS_EMSI_CLIENT_RESPONSE
 from test_utils.testcase import TaxonomyTestCase
@@ -204,6 +204,79 @@ class TestUtils(TaxonomyTestCase):
         assert utils.is_skill_blacklisted(PROGRAM_UUID, self.skill.id, product_type) is False
         assert models.ProgramSkill.objects.filter(
             program_uuid=PROGRAM_UUID,
+            skill=self.skill,
+            is_blacklisted=False,
+        ).exists()
+
+        # Make sure the `Skill` object updated
+        self.skill.refresh_from_db()
+        assert self.skill.name == skill_data.get('name')
+        assert self.skill.info_url == skill_data.get('info_url')
+        assert self.skill.type_id == skill_data.get('type_id')
+        assert self.skill.type_name == skill_data.get('type_name')
+        assert self.skill.description == skill_data.get('description')
+
+    def test_update_xblock_skills_data(self):
+        """
+        Validate that update_xblock_skills_data works as expected.
+        """
+        xblock = factories.XblockSkillsFactory(usage_key=USAGE_KEY)
+        black_listed_xblock_skill = factories.XblockSkillThroughFactory(xblock=xblock, is_blacklisted=True)
+        skills_count = Skill.objects.count()
+        product_type = ProductTypes.Xblock
+        utils.update_skills_data(
+            key_or_uuid=USAGE_KEY,
+            skill_external_id=black_listed_xblock_skill.skill.external_id,
+            confidence=black_listed_xblock_skill.confidence,
+            skill_data={
+                'name': black_listed_xblock_skill.skill.name,
+                'info_url': black_listed_xblock_skill.skill.info_url,
+                'type_id': black_listed_xblock_skill.skill.type_id,
+                'type_name': black_listed_xblock_skill.skill.type_name,
+                'description': black_listed_xblock_skill.skill.description
+            },
+            product_type=product_type
+        )
+
+        updated_name = 'new_name'
+        updated_info_url = 'new_url'
+        updated_type_id = '1'
+        updated_type_name = 'new_type'
+        updated_description = 'new description'
+        skill_data = {
+            'name': updated_name,
+            'info_url': updated_info_url,
+            'type_id': updated_type_id,
+            'type_name': updated_type_name,
+            'description': updated_description
+        }
+        utils.update_skills_data(
+            key_or_uuid=USAGE_KEY,
+            skill_external_id=self.skill.external_id,
+            confidence=0.9,
+            skill_data=skill_data,
+            product_type=product_type
+        )
+
+        # make sure no new `Skill` object created.
+        assert Skill.objects.count() == skills_count
+
+        # Make sure `XblockSkills` is not removed from the blacklist.
+        assert utils.is_skill_blacklisted(
+            xblock.id,
+            black_listed_xblock_skill.skill.id,
+            ProductTypes.XblockThrough,
+        ) is True
+        xblock_skill = models.XblockSkillThrough.objects.get(
+            xblock=xblock,
+            skill=black_listed_xblock_skill.skill,
+        )
+        assert xblock_skill.is_blacklisted is True
+
+        # Make sure that skill that was not black listed is added with no issues.
+        assert utils.is_skill_blacklisted(xblock.id, self.skill.id, ProductTypes.XblockThrough) is False
+        assert models.XblockSkillThrough.objects.filter(
+            xblock=xblock,
             skill=self.skill,
             is_blacklisted=False,
         ).exists()
