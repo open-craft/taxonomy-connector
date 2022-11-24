923ebaa150c5af86deb7fae1d50525e11567710f
Pooja Kulkarni <pooja@opencraft.com>
2022-11-24 15:17:14 -0500

    fix: update field names and add tests

--- taxonomy/management/commands/finalize_skill_tags.py
+++ taxonomy/management/commands/finalize_skill_tags.py
@@ -6,7 +6,6 @@ Management command for finalizing the skill tags based on number of votes.
 import logging
 
 from django.core.management.base import BaseCommand
-from django.utils.translation import gettext as _
 
 from taxonomy.constants import MIN_VOTES_FOR_SKILLS, RATIO_THRESHOLD_FOR_SKILLS
 from taxonomy.models import XBlockSkillData
@@ -29,9 +28,9 @@ class Command(BaseCommand):
         LOGGER.info('Starting skills verification task')
         unverified_skills = XBlockSkillData.objects.filter(verified=False)
         for xblock_skill in unverified_skills:
-            has_valid_votes = bool(xblock_skill.relevant_count > MIN_VOTES_FOR_SKILLS)
-            total_count = int(xblock_skill.relavant_count + xblock_skill.irrelevant_count)
-            count_ratio = float(xblock_skill.relevant_count / total_count)
+            has_valid_votes = bool(xblock_skill.verified_count > MIN_VOTES_FOR_SKILLS)
+            total_count = int(xblock_skill.verified_count + xblock_skill.ignored_count)
+            count_ratio = float(xblock_skill.verified_count / total_count)
             crosses_ratio_threshold = bool(count_ratio > RATIO_THRESHOLD_FOR_SKILLS)
             if has_valid_votes and crosses_ratio_threshold:
                 xblock_skill.verified = True
